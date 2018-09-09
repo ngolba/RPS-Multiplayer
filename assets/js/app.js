af5032a3e02576216e0e1d6928ac14e311119e3a
Nathan Golba <ngolba@gmail.com>
2018-09-09 03:10:07 -0400

    file structure

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+     apiKey: "AIzaSyBKVfQM8kE7hpS0dksP7uJlkPJ_q_N_-yQ",
+     authDomain: "pvp-rps.firebaseapp.com",
+     databaseURL: "https://pvp-rps.firebaseio.com",
+     projectId: "pvp-rps",
+     storageBucket: "",
+     messagingSenderId: "742631516397"
+ };
+ firebase.initializeApp(config);
